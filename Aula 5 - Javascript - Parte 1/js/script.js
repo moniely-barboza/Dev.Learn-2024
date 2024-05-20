f96244f3d4542b304621f5e23a569a8eb9a0fcf4
Moniely Barboza <moniely.barboza@usp.br>
2024-05-20 14:42:35 -0300

    Aula 05 - Javascript - Parte 1

---
+++ Aula 5 - Javascript - Parte 1/js/script.js
@@ -0,0 +1,7 @@
+function changeColor(id, color) {
+    document.querySelector(id).style.color = color; 
+}
+
+function changeBackgroundColor(id, color) {
+    document.querySelector(id).style.backgroundColor = color; 
+}
